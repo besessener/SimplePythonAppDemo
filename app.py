be777cf1148a9ae59b30641b32c820fccc5d871d
Matthias Eggert <matthias.eggert@qytera.de>
2023-10-13 10:39:05 +0200

    add other resources, add sleep variable

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 import time
-import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # simulating load with random sleep
-    sleep_time = random.uniform(0, 1)
+    # Retrieve the 'sleep' parameter from the URL, default to 0 if not provided
+    sleep_time = float(request.args.get('sleep', 0))
+    
+    # Introduce a delay based on the sleep_time value
     time.sleep(sleep_time)
-    return "Hello World!"
+
+    return f"Slept for {sleep_time} seconds."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
