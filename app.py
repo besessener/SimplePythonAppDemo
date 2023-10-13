b0b64aa1e5b472be28a8e25f1b464b9fbfd56992
Matthias Eggert <matthias.eggert@qytera.de>
2023-10-13 08:58:45 +0200

    add sleep

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import time
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # simulating load with random sleep
+    sleep_time = random.uniform(0, 1)
+    time.sleep(sleep_time)
     return "Hello World!"
 
 if __name__ == '__main__':
