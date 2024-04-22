820fc4f5254e686d66c68ade359213c8168cc2de
Matthias Eggert <matthias.eggert@qytera.de>
2024-04-22 13:07:11 +0200

    update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import asyncio
+import os
 
 app = FastAPI()
 
 @app.get("/hello")
 async def hello(sleep: int = 0, size_kb: int = 0):
-    await asyncio.sleep(sleep)
+    await asyncio.sleep(sleep / 1000)  # Sleep for the requested amount of time (in milliseconds)
     
     # Generate a random string of the requested size in KB
     response = os.urandom(size_kb * 1024).hex()  # os.urandom returns random bytes, which are then converted to a hex string
