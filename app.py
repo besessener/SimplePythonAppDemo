b1e2bd9fe2634160873e01abb74a51de1f609830
Matthias Eggert <matthias.eggert@qytera.de>
2024-04-22 11:39:58 +0200

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
 import asyncio
-from flask_asyncio import AsyncFlask
+from quart import Quart, request
+import os
 
-app = AsyncFlask(__name__)  # Use AsyncFlask instead of Flask
+app = Quart(__name__)  # Use Quart instead of Flask
 
 @app.route('/')
 async def hello():
@@ -10,7 +10,7 @@ async def hello():
     sleep_time = float(request.args.get('sleep', 0))
     
     # Introduce a delay based on the sleep_time value
-    await asyncio.sleep(sleep_time / 1000)
+    await asyncio.sleep(sleep_time / 1000)  # async sleep instead of time.sleep
 
     # Retrieve the 'size_kb' parameter from the URL, default to 0 if not provided
     size_kb = int(request.args.get('size_kb', 0))
@@ -19,3 +19,6 @@ async def hello():
     response = os.urandom(size_kb * 1024).hex()  # os.urandom returns random bytes, which are then converted to a hex string
 
     return response
+
+if __name__ == '__main__':
+    app.run()
